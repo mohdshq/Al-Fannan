94b0ddc2bdea04789ca488776da8b0721ee4d04d
Mohammed Alshaqra <mohd.78@hotmail.com>
2026-05-05 13:47:36 +0400

    Initial Commit

---
+++ Al Fannan/Al_FannanApp.swift
@@ -0,0 +1,17 @@
+//
+//  Al_FannanApp.swift
+//  Al Fannan
+//
+//  Created by Mohammed Alshaqra on 05/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct Al_FannanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Al Fannan/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Al Fannan
+//
+//  Created by Mohammed Alshaqra on 05/05/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
